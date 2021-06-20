2cd7cc5b251b86a20ed725dda9a72e70d2e8d4f3
Kenn Stack <kibadikennedy@gmail.com>
2021-06-21 03:25:54 +0800

    added components

---
+++ src/Components/HomeComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const HomeComponent = () => {
+    return (
+        <div>
+            <div className="text-center my-4">
+            {/* Logo */}
+                <h1 className="text-3xl font-semibold">LifePay</h1>
+                <p className="text-lg font-medium text-gray-600 my-4">Quick Pay. Quick Offer.</p>
+            </div>
+
+            {/* Payments List */}
+            <div>
+                {/* WeChat Pay */}
+                {/* AliPay */}
+                {/* PayPal */}
+            </div>
+            {/* Footer */}
+        </div>
+    )
+}
+
+export default HomeComponent
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import HomeComponent from '../Components/HomeComponent'
 
-export default function Home() {
-  return <div>Hello world!</div>
+function Home() {
+  return (
+    <div>
+      <HomeComponent/>
+    </div>
+  )
 }
+
+export default Home
