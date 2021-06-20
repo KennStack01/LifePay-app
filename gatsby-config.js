2a30913d7543ed60b25006e6891f138de701b290
Kenn Stack <kibadikennedy@gmail.com>
2021-06-21 05:52:46 +0800

    added contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,23 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 module.exports = {
   /* Your site config here */
   plugins: [
-    'gatsby-plugin-postcss'
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
