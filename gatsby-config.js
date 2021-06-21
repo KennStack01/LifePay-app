0129aba69af663e8a1e017f7ade281247de527c2
Kenn Stack <kibadikennedy@gmail.com>
2021-06-22 00:04:18 +0800

    react helmet features

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,20 @@ require("dotenv").config({
 
 module.exports = {
   /* Your site config here */
+  /* Your site config here */
+  siteMetadata: {
+    title: `LifePay App`,
+    siteUrl: `https://www/lifepay.app`,
+    author: "@LifeWordNation",
+    description: `LifeWord Nation`,
+    keywords: `Payments Gates, WeChat Pay, AliPay, PayPal, Bank Transfer`,
+    image: "/Images/Picture-Cover.jpg", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@PenielKatombe",
+  },
   plugins: [
     'gatsby-plugin-postcss',
     `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
